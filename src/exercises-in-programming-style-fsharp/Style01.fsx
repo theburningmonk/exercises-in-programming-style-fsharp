053088557ce108e1f3926c71600974b458a41a71
theburningmonk <theburningmonk@gmail.com>
2015-08-26 02:40:05 +0100

    - wip on style 1

--- src/exercises-in-programming-style-fsharp/Style01.fsx
+++ src/exercises-in-programming-style-fsharp/Style01.fsx
@@ -28,14 +28,64 @@ STRATEGY
 (Part 2)
 4. find the 25 most frequent words in secondary memory (file)
 *)
-let stopwords = 
-    File.ReadAllText(__SOURCE_DIRECTORY__ + "../stop_words.txt")
-        .Split(',')
+let stopWords = 
+    File.ReadAllText(__SOURCE_DIRECTORY__ + "../stop_words.txt").Split(',')
+    |> Set.ofArray
+
+let wordFreqs = File.Open(__SOURCE_DIRECTORY__ + "../word_freqs.txt", 
+                          FileMode.Create, 
+                          FileAccess.ReadWrite)
 
 //let input = new StreamReader(``pride and prejudice``)
 let input = new StreamReader(test)
+let next  = input.ReadLine
+let (|IsAlnum|_|) c = if System.Char.IsLetterOrDigit c then Some () else None
+let (|Int|) input = System.Int32.Parse input
 
-let rec loop = function
-    | null -> ()
-    | line -> ()
-loop <| input.ReadLine()
+// very imperative, but the goal is to work with min amount of memory
+let findWords (line : string) =
+    seq {
+        let mutable startIdx, len = -1, 0
+        for i = 0 to line.Length-1 do
+            match line.[i], startIdx with
+            | IsAlnum, -1 -> startIdx <- i; len <- 1   // start of word
+            | IsAlnum, _  -> len <- len + 1            // another char for word
+            | _, -1 -> () // no word, not started, ignore
+            | _ -> // end of word
+                yield line.Substring(startIdx, len)
+                startIdx <- -1; len <- 0
+
+        if len > 0 then yield line.Substring(startIdx, len)
+    }
+    |> Seq.filter (fun x -> x.Length >= 2 && stopWords.Contains x |> not)
+
+let write (stream : Stream) word count = 
+    use writer = new StreamWriter(stream, System.Text.Encoding.ASCII, 1024, true)
+    writer.WriteLine(sprintf "%s,%d" word count)
+    writer.Flush()
+
+let incrFreq (word : string) =
+    wordFreqs.Seek(0L, SeekOrigin.Begin) |> ignore
+    use reader = new StreamReader(wordFreqs, System.Text.Encoding.ASCII, false, 1024, true)
+
+    let rec loop : string -> bool = function
+        | null -> false // end of file, word not found
+        | line -> 
+            let [| word'; (Int count) |] = line.Split(',')
+            if word' = word then 
+                wordFreqs.Seek(-int64 line.Length, SeekOrigin.Current) |> ignore
+                write wordFreqs word' (count + 1)
+                true
+            else loop <| reader.ReadLine()
+
+    let found = loop <| reader.ReadLine()
+    if not found then write wordFreqs word 1
+
+let rec proc (next : unit -> string) =
+    match next() with
+    | null -> () // end of file
+    | line -> 
+        findWords line |> Seq.iter incrFreq
+        proc next
+
+proc next
