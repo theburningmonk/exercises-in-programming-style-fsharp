2c183319f40cdf7a7026e5ca8d2d8eada3d873d7
theburningmonk <theburningmonk@gmail.com>
2015-09-16 23:55:41 +0100

    - don't use a dictionary

--- src/exercises-in-programming-style-fsharp/Style03.fsx
+++ src/exercises-in-programming-style-fsharp/Style03.fsx
@@ -1,10 +1,9 @@
 ﻿open System
-open System.Collections.Generic
 open System.IO
 
 #time
 
-let wordFreqs = new Dictionary<string, int>()
+let mutable wordFreqs = Array.empty<string * int>
 let stopWords = 
     File.ReadAllText(__SOURCE_DIRECTORY__ + "../stop_words.txt")
         .Split(',')
@@ -13,33 +12,37 @@ let ``pride and prejudice`` = __SOURCE_DIRECTORY__ + "../pride-and-prejudice.txt
 let test = __SOURCE_DIRECTORY__ + "../test.txt"
 
 for line in File.ReadAllLines(``pride and prejudice``) do
-    let mutable startChar = None
-    let mutable i = 0
-    let mutable word  = ""
+    let mutable startIdx = None
+    let mutable idx  = 0
+    let mutable word = ""
 
     for c in line do
-        if startChar.IsNone then
+        if startIdx.IsNone then
             if Char.IsLetterOrDigit c then
                 // we found the start of a word
-                startChar <- Some i
+                startIdx <- Some idx
         else
             if not <| Char.IsLetterOrDigit c then
                 // we found the end of a word, process it
-                word  <- line.[startChar.Value..i-1].ToLower()
+                word  <- line.[startIdx.Value..idx-1].ToLower()
 
-                if stopWords |> Array.forall ((<>) word) 
-                   && word.Length >= 2   
+                if stopWords |> Array.forall ((<>) word) && word.Length >= 2
                 then
-                    if not <| wordFreqs.ContainsKey word then
-                         wordFreqs.[word]  <- 1
-                    else wordFreqs.[word] <- wordFreqs.[word] + 1
+                    let wordFreqIdx = 
+                        wordFreqs 
+                        |> Array.tryFindIndex (fun (word', _) -> word = word')
+                        
+                    match wordFreqIdx with
+                    | None -> wordFreqs <- Array.append wordFreqs [| word, 1 |]
+                    | Some idx -> 
+                        let word, count = wordFreqs.[idx]
+                        wordFreqs.[idx] <- word, count + 1
 
                 // reset
-                startChar <- None
+                startIdx <- None
 
-        i <- i+1
+        idx <- idx+1
 
-wordFreqs 
-|> Seq.sortByDescending (fun kvp -> kvp.Value)
-|> Seq.take 25
-|> Seq.iter (fun kvp -> printfn "%s - %d" kvp.Key kvp.Value)
+wordFreqs |> Array.sortInPlaceBy (fun (_, count) -> -count)
+wordFreqs.[0..24] 
+|> Array.iter (fun (word, count) -> printfn "%s - %d" word count)
