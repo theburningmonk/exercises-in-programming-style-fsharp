e7c47788a487af25b07574c9ee043a4c287236f6
theburningmonk <theburningmonk@gmail.com>
2015-09-17 02:22:09 +0100

    - fixed bug with Style 3 where words at the end of a line wasn't counted

--- src/exercises-in-programming-style-fsharp/Style03.fsx
+++ src/exercises-in-programming-style-fsharp/Style03.fsx
@@ -12,36 +12,39 @@ let ``pride and prejudice`` = __SOURCE_DIRECTORY__ + "../pride-and-prejudice.txt
 let test = __SOURCE_DIRECTORY__ + "../test.txt"
 
 for line in File.ReadAllLines(``pride and prejudice``) do
+    // File.ReadAllLines trims line ending, which causes special case in our
+    // code below that searches for words, so easier to add newline character
+    // even though it's not efficient
+    let line = line + "\n"
+
     let mutable startIdx = None
-    let mutable idx  = 0
     let mutable word = ""
 
-    for c in line do
+    for idx = 0 to line.Length-1 do
+        let c = line.[idx]
+
         if startIdx.IsNone then
             if Char.IsLetterOrDigit c then
                 // we found the start of a word
                 startIdx <- Some idx
-        else
-            if not <| Char.IsLetterOrDigit c then
-                // we found the end of a word, process it
-                word  <- line.[startIdx.Value..idx-1].ToLower()
+        elif not <| Char.IsLetterOrDigit c then
+            // we found the end of a word, process it
+            word <- line.[startIdx.Value..idx-1].ToLower()
 
-                if stopWords |> Array.forall ((<>) word) && word.Length >= 2
-                then
-                    let wordFreqIdx = 
-                        wordFreqs 
-                        |> Array.tryFindIndex (fun (word', _) -> word = word')
+            if stopWords |> Array.forall ((<>) word) && word.Length >= 2
+            then
+                let wordFreqIdx = 
+                    wordFreqs 
+                    |> Array.tryFindIndex (fun (word', _) -> word = word')
                         
-                    match wordFreqIdx with
-                    | None -> wordFreqs <- Array.append wordFreqs [| word, 1 |]
-                    | Some idx -> 
-                        let word, count = wordFreqs.[idx]
-                        wordFreqs.[idx] <- word, count + 1
+                match wordFreqIdx with
+                | None -> wordFreqs <- Array.append wordFreqs [| word, 1 |]
+                | Some idx -> 
+                    let word, count = wordFreqs.[idx]
+                    wordFreqs.[idx] <- word, count + 1
 
-                // reset
-                startIdx <- None
-
-        idx <- idx+1
+            // reset
+            startIdx <- None
 
 wordFreqs |> Array.sortInPlaceBy (fun (_, count) -> -count)
 wordFreqs.[0..24] 
