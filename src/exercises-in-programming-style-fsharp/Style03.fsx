039998890098e1e1aa6480589c0dc02b227bdf46
theburningmonk <theburningmonk@gmail.com>
2015-09-19 15:18:22 +0100

    - made lines shorter in style 3

--- src/exercises-in-programming-style-fsharp/Style03.fsx
+++ src/exercises-in-programming-style-fsharp/Style03.fsx
@@ -8,13 +8,14 @@ let stopWords =
     File.ReadAllText(__SOURCE_DIRECTORY__ + "../stop_words.txt")
         .Split(',')
 
-let ``pride and prejudice`` = __SOURCE_DIRECTORY__ + "../pride-and-prejudice.txt"
+let ``pride and prejudice`` = 
+    __SOURCE_DIRECTORY__ + "../pride-and-prejudice.txt"
 let test = __SOURCE_DIRECTORY__ + "../test.txt"
 
 for line in File.ReadAllLines(``pride and prejudice``) do
-    // File.ReadAllLines trims line ending, which causes special case in our
-    // code below that searches for words, so easier to add newline character
-    // even though it's not efficient
+    // File.ReadAllLines trims line ending, which causes special 
+    // case in our code below that searches for words, so easier 
+    // to add newline character even though it's not efficient
     let line = line + "\n"
 
     let mutable startIdx = None
@@ -31,14 +32,15 @@ for line in File.ReadAllLines(``pride and prejudice``) do
             // we found the end of a word, process it
             word <- line.[startIdx.Value..idx-1].ToLower()
 
-            if stopWords |> Array.forall ((<>) word) && word.Length >= 2
+            if stopWords |> Array.forall ((<>) word) 
+               && word.Length >= 2
             then
                 let wordFreqIdx = 
-                    wordFreqs 
-                    |> Array.tryFindIndex (fun (word', _) -> word = word')
+                    wordFreqs |> Array.tryFindIndex (fst >> (=) word)
                         
                 match wordFreqIdx with
-                | None -> wordFreqs <- Array.append wordFreqs [| word, 1 |]
+                | None -> 
+                    wordFreqs <- Array.append wordFreqs [| word, 1 |]
                 | Some idx -> 
                     let word, count = wordFreqs.[idx]
                     wordFreqs.[idx] <- word, count + 1
