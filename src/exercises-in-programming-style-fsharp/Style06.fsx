8c43136907c253becc303fea594d21d611b03845
theburningmonk <theburningmonk@gmail.com>
2015-10-09 01:30:50 +0100

    - style 6 down to 3 lines

--- src/exercises-in-programming-style-fsharp/Style06.fsx
+++ src/exercises-in-programming-style-fsharp/Style06.fsx
@@ -4,8 +4,6 @@ open System.Text.RegularExpressions
 
 #time
 
-let stops = (File.ReadAllText <| __SOURCE_DIRECTORY__ + "../stop_words.txt").Split(',') |> Set.ofArray
-let words = Regex.Matches((File.ReadAllText <| __SOURCE_DIRECTORY__ + "../pride-and-prejudice.txt").ToLower(), "[a-z]{2,}") 
-            |> Seq.cast<Match> |> Seq.map (fun m -> m.Value)
-let counts = words.Where(stops.Contains >> not).GroupBy(fun x -> x).Select(fun gr -> gr.Key, gr |> Seq.length)
-counts |> Seq.sortByDescending snd |> Seq.take 25 |> Seq.iter (fun (w, n) -> printfn "%s - %d" w n)
+let s = (File.ReadAllText <| __SOURCE_DIRECTORY__ + "../stop_words.txt").Split(',') |> Set.ofArray
+let w = Regex.Matches((File.ReadAllText <| __SOURCE_DIRECTORY__ + "../pride-and-prejudice.txt").ToLower(), "[a-z]{2,}") |> Seq.cast<Match> |> Seq.map (fun m -> m.Value)
+w.Where(s.Contains >> not).GroupBy(fun x -> x).Select(fun gr -> gr.Key, gr |> Seq.length) |> Seq.sortByDescending snd |> Seq.take 25 |> Seq.iter (fun (w, n) -> printfn "%s - %d" w n)
