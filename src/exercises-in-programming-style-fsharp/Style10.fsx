e36f69817dcebad59dcf0434d0e0705c72563a52
Yan Cui <theburningmonk@gmail.com>
2015-10-15 16:22:15 +0100

    - added style 10 - wip on style 11

---
+++ src/exercises-in-programming-style-fsharp/Style10.fsx
@@ -0,0 +1,55 @@
+﻿open System
+open System.Collections.Generic
+open System.IO
+open System.Text.RegularExpressions
+
+#time
+
+let ``stop words`` = 
+    __SOURCE_DIRECTORY__ + "/stop_words.txt"
+let ``p & p`` = 
+    __SOURCE_DIRECTORY__ + "/pride-and-prejudice.txt"
+
+type DataStorageManager (filepath) =
+    let regex = new Regex("[\W_]+")
+    let data  = regex.Replace(File.ReadAllText(filepath), " ").ToLower()
+
+    member __.Words = data.Split()
+
+type StopWordsManager () =
+    let stopWords = 
+        File.ReadAllText(``stop words``)
+        |> (fun s -> s.Split(','))
+        |> Array.append ([|'a'..'z'|] |> Array.map string)
+        |> Set.ofArray
+
+    member __.IsStopWord word = stopWords.Contains word
+
+type WordFrequencyManager () =
+    let wordFreqs = Dictionary<string, int>()
+
+    member __.IncrementCount word =
+        match wordFreqs.TryGetValue word with
+        | true, n -> wordFreqs.[word] <- n+1
+        | _ -> wordFreqs.[word] <- 1
+
+    member __.Sorted () =
+        wordFreqs
+        |> Seq.map (fun (KeyValue(word, n)) -> word, n)
+        |> Seq.sortBy (fun (word, n) -> -n)
+
+type WordFrequencyController (filePath) =
+    let dataStorageManager = DataStorageManager(filePath)
+    let stopWordsManager   = StopWordsManager()
+    let wordFreqsManager   = WordFrequencyManager()
+
+    member __.Run () =
+        dataStorageManager.Words
+        |> Array.filter (stopWordsManager.IsStopWord >> not)
+        |> Array.iter wordFreqsManager.IncrementCount
+
+        wordFreqsManager.Sorted()
+        |> Seq.take 25
+        |> Seq.iter (fun (word, n) -> printfn "%s - %d" word n)
+
+WordFrequencyController(``p & p``).Run()
---
+++ src/exercises-in-programming-style-fsharp/Style11.fsx
@@ -0,0 +1,32 @@
+﻿open System
+open System.IO
+open System.Text.RegularExpressions
+
+#time
+
+let ``stop words`` = 
+    __SOURCE_DIRECTORY__ + "/stop_words.txt"
+let ``p & p`` = 
+    __SOURCE_DIRECTORY__ + "/pride-and-prejudice.txt"
+
+type DataStorageManager () =
+    let mutable data = ""
+
+    let init filePath =
+        let regex = new Regex("[\W_]+")
+        data <- regex.Replace(File.ReadAllText(filePath), " ").ToLower()
+
+    let words () = data.Split()
+
+    member __.Dispatch (message : string[]) : obj =
+       match message with
+       | [| "init"; filePath |] -> init filePath; null
+       | [| "words" |] -> words () :> obj
+
+type StopWordsManager () =
+    let mutable stopWords = [||]
+
+    let init () =
+        stopWords <- File.ReadAllText(``stop words``).Split(',')
+        stopWords <- Array.append stopWords ([|'a'..'z'|] |> Array.map string)
+        |> Set.ofArray
