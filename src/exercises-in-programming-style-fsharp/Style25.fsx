8e67def878c3ac32587272b0adea35b80ecfd2af
Yan Cui <theburningmonk@gmail.com>
2016-03-02 11:21:36 +0000

    - added style 28

--- src/exercises-in-programming-style-fsharp/Style25.fsx
+++ src/exercises-in-programming-style-fsharp/Style25.fsx
@@ -1,1 +1,33 @@
-﻿
+﻿#r "../../packages/SQLProvider/lib/FSharp.Data.SqlProvider.dll"
+
+open FSharp.Data.Sql
+open System.IO
+
+#time
+
+let ``stop words`` = __SOURCE_DIRECTORY__ + "../stop_words.txt"
+let ``p & p`` = __SOURCE_DIRECTORY__ + "../pride-and-prejudice.txt"
+
+let [<Literal>] resolutionPath = __SOURCE_DIRECTORY__ + @"..\..\files\sqlite" 
+let [<Literal>] connectionString = "Data Source=" + __SOURCE_DIRECTORY__ + @"\northwindEF.db;Version=3"
+// create a type alias with the connection string and database vendor settings
+type sql = SqlDataProvider< 
+              ConnectionString = connectionString,
+              DatabaseVendor = Common.DatabaseProviderTypes.SQLITE,
+              ResolutionPath = resolutionPath,
+              IndividualsAmount = 1000,
+              UseOptionTypes = true >
+let ctx = sql.GetDataContext()
+
+let createDbSchema (conn : SQLiteConnection) =
+    let query = """
+CREATE TABLE documents (id INTEGER PRIMARY KEY AUTOINCREMENT, name);
+CREATE TABLE words (id, doc_id, value);
+CREATE TABLE characters (id, word_id, value);
+"""
+    use cmd = new SQLiteCommand(query, conn)
+    cmd.ExecuteNonQuery()
+
+SQLiteConnection.CreateFile("p&p.sqlite")
+let conn = new SQLiteConnection("Data Source=p&p.sqlite")
+
---
+++ src/exercises-in-programming-style-fsharp/Style28.fsx
@@ -0,0 +1,151 @@
+﻿open System.Collections.Generic
+open System.IO
+open System.Text.RegularExpressions
+
+#time
+
+let ``stop words`` = 
+    __SOURCE_DIRECTORY__ + "/stop_words.txt"
+let ``p & p`` = 
+    __SOURCE_DIRECTORY__ + "/pride-and-prejudice.txt"
+
+type DataStorageMessage =
+    | LoadWords of string
+    | NextWord  of AsyncReplyChannel<string option>
+
+let dataStorageManager = 
+    MailboxProcessor<DataStorageMessage>.Start(fun inbox ->
+        let loadWords path =
+            Regex("[\W_]+")
+                .Replace(File.ReadAllText path, " ")
+                .ToLower()
+                .Split()
+            |> Array.toList
+
+        let rec loop data = 
+            async {
+                let! msg = inbox.Receive()
+                match msg with
+                | LoadWords path ->
+                    return! loop <| loadWords path
+                | NextWord reply ->
+                    match data with
+                    | [] -> 
+                        reply.Reply None
+                        return! loop data
+                    | hd::tl -> 
+                        reply.Reply <| Some hd
+                        return! loop tl
+            }
+
+        loop [])
+
+type StopWordsMessage =
+    | LoadStopWords of string
+    | IsStopWord    of string * AsyncReplyChannel<bool>
+
+let stopWordsManager = 
+    MailboxProcessor<StopWordsMessage>.Start(fun inbox ->
+        let loadStopWords path =
+            File.ReadAllText path
+            |> (fun s -> s.Split(','))
+            |> Array.append 
+                ([|'a'..'z'|] |> Array.map string)
+            |> Set.ofArray
+
+        let rec loop stopWords =
+            async {
+                let! msg = inbox.Receive()
+                match msg with
+                | LoadStopWords path ->  
+                    return! loop <| loadStopWords path
+                | IsStopWord (word, reply) ->
+                    reply.Reply <| stopWords.Contains word
+                    return! loop stopWords
+            }
+            
+        loop <| set [])
+
+type WordFrequencyMessage =
+    | Add   of string
+    | TopN  of int * AsyncReplyChannel<(string * int)[]>
+    | Reset
+
+let wordFreqManager =
+    MailboxProcessor<WordFrequencyMessage>.Start(fun inbox ->
+        let wordFreqs = Dictionary<string, int>()
+        
+        async {
+            while true do
+                let! msg = inbox.Receive()
+                match msg with
+                | Reset -> wordFreqs.Clear()
+                | Add word ->
+                    match wordFreqs.TryGetValue word with
+                    | true, n -> wordFreqs.[word] <- n+1
+                    | _       -> wordFreqs.[word] <- 1
+                | TopN (n, reply) ->
+                    wordFreqs
+                    |> Seq.map (fun (KeyValue(word, n)) -> word, n)
+                    |> Seq.sortByDescending snd
+                    |> Seq.take n
+                    |> Seq.toArray
+                    |> reply.Reply
+        
+        })
+
+type ControllerMessage =
+    | Run of AsyncReplyChannel<unit>
+
+let controller =
+    MailboxProcessor<ControllerMessage>.Start(fun inbox ->
+        let init () =
+            dataStorageManager.Post <| LoadWords ``p & p``
+            stopWordsManager.Post <| LoadStopWords ``stop words``
+            wordFreqManager.Post Reset
+
+        let rec countWords () =
+            async {
+                let! word = 
+                    dataStorageManager.PostAndAsyncReply NextWord
+
+                match word with
+                | None -> ()
+                | Some word ->
+                    let! ignore = 
+                        stopWordsManager
+                            .PostAndAsyncReply(fun reply -> 
+                                IsStopWord(word, reply))
+                    
+                    if not ignore then
+                        wordFreqManager.Post <| Add word
+
+                    return! countWords ()
+            }
+
+        let printTop25 () =
+            async {
+                let! top25 = 
+                    wordFreqManager
+                        .PostAndAsyncReply(fun reply ->
+                            TopN(25, reply))
+                
+                top25
+                |> Array.iter (fun (word, n) -> 
+                printfn "%s - %d" word n)
+            }
+
+        async {
+            let! msg = inbox.Receive()
+            match msg with
+            | Run reply ->
+                init()
+                do! countWords()
+                do! printTop25()
+
+                reply.Reply ()
+
+        })
+
+controller.PostAndAsyncReply Run
+|> Async.RunSynchronously
